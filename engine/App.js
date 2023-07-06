c86dc71e02c606df887bbfc3051623be5f6fd7bd
Damir Tagilbayev <93281431+damirTAG@users.noreply.github.com>
2023-07-07 03:16:45 +0600

    ru/en translate

--- engine/App.js
+++ engine/App.js
@@ -12,3 +12,37 @@ $.getJSON("https://api.ipify.org?format=json", function (data) {
 let author = "DAMIRTAG'S PORTFOLIO, ";
 let github = "GITHUB REPO: HTTPS://GITHUB.COM/DAMIRTAG/PORTFOLIO";
 console.log(author + github);
+
+var arrLang = {
+    "en-gb": {
+        me: "Я — Веб Разработчик",
+        devfor: "Разработаю для Вас сайт",
+        status: "Статус разработки",
+        techs: "Технологии которые я использую",
+        design: "Дизайн",
+    },
+    "zh-tw": {
+        me: "I am — Web Developer",
+        devfor: "Turnkey website development",
+        status: "Development status",
+        techs: "Technologies I Use",
+        design: "Design",
+    },
+};
+
+$(document).ready(function () {
+    // The default language is English
+    var lang = "en-gb";
+    $(".lang").each(function (index, element) {
+        $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+});
+
+// get/set the selected language
+$(".translate").click(function () {
+    var lang = $(this).attr("id");
+
+    $(".lang").each(function (index, element) {
+        $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+});
