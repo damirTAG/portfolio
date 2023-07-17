eb9742fa2be6809d24d0173c218f20b6fa25f0fc
Damir Tagilbayev <93281431+damirTAG@users.noreply.github.com>
2023-07-17 23:26:22 +0600

    Add files via upload

--- engine/App.js
+++ engine/App.js
@@ -15,14 +15,38 @@ let author = "DAMIRTAG'S PORTFOLIO, ";
 let github = "GITHUB REPO: HTTPS://GITHUB.COM/DAMIRTAG/PORTFOLIO";
 console.log(author + github);
 //swiper.js begin
+// var swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//         el: ".swiper-pagination",
+//         type: "progressbar",
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
 var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: true,
+    },
     pagination: {
         el: ".swiper-pagination",
-        type: "progressbar",
+        clickable: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+    on: {
+        autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        },
     },
 });
 //swiper.js end
@@ -48,3 +72,52 @@ function displayText1() {
     }
 }
 // ---------
+//PHONE NUMBER SKELET START
+document.addEventListener("DOMContentLoaded", function () {
+    var eventCalllback = function (e) {
+        var el = e.target,
+            clearVal = el.dataset.phoneClear,
+            pattern = el.dataset.phonePattern,
+            matrix_def = "+7(___) ___-__-__",
+            matrix = pattern ? pattern : matrix_def,
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = e.target.value.replace(/\D/g, "");
+        if (clearVal !== "false" && e.type === "blur") {
+            if (val.length < matrix.match(/([\_\d])/g).length) {
+                e.target.value = "";
+                return;
+            }
+        }
+        if (def.length >= val.length) val = def;
+        e.target.value = matrix.replace(/./g, function (a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+        });
+    };
+    var phone_inputs = document.querySelectorAll("[data-phone-pattern]");
+    for (let elem of phone_inputs) {
+        for (let ev of ["input", "blur", "focus"]) {
+            elem.addEventListener(ev, eventCalllback);
+        }
+    }
+});
+//PHONE NUM SKELET END
+//alert success jquery
+$(document).ready(function () {
+    $("form").submit(function () {
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "https://formsubmit.co/8b33eca9175fd2ddb1c8130375e39149", //Changed
+            data: th.serialize(),
+        }).done(function () {
+            alert("Успешно!");
+            setTimeout(function () {
+                // Done Functions
+                th.trigger("reset");
+            }, 300);
+        });
+        return false;
+    });
+});
+//alert end
